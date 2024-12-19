1efbe04f23a764e4f0da9c899980c3601420a748
randale-porciuncula <onedaleporciuncula@gmail.com>
2024-12-19 20:11:57 +0800

    initial layout of album

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <h1>hello</h1>
+      <Link href='/portfolio/brc'>BRC Track Photos</Link>
+    </>
+  );
+}
