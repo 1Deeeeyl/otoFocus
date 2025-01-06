cb2bb8cb5d6be2abf6e299151cfce15ca013d07e
randale-porciuncula <onedaleporciuncula@gmail.com>
2025-01-06 18:49:52 +0800

    minor bug fix

--- src/components/galleryList/GalleryList.tsx
+++ src/components/galleryList/GalleryList.tsx
@@ -8,8 +8,8 @@ export default function page() {
   return (
     <>
       {gallery.map((album, index) => (
-        <CardContainer className="flex-grow " key={index}>
-          <CardBody className="bg-gray-50 relative group/card    border-black/[0.1] min-w-[20rem] h-auto rounded-xl border  xl:w-[614px] lg:w-[30rem]">
+        <CardContainer className="flex-grow" key={index}>
+          <CardBody className="bg-gray-50 relative group/card    border-black/[0.1] min-w-[20rem] h-auto rounded-xl border  xl:w-[604px] lg:w-[30rem]">
             <CardItem
               translateZ="50"
               className="text-xl font-bold text-neutral-600  px-6 pt-6"
@@ -18,6 +18,7 @@ export default function page() {
             </CardItem>
 
             <CardItem translateZ="100" className="w-full pt-6">
+              <Link href={`portfolio/${album.albumId}`}>
               <Image
                 src={album.cover}
                 width="4000"
@@ -25,14 +26,17 @@ export default function page() {
                 className="h-60 w-full object-cover group-hover/card:shadow-xl"
                 alt="thumbnail"
                 quality={100}
+                placeholder="blur"
+                blurDataURL={album.blurDataURL}
               />
+              </Link>
             </CardItem>
             <div className="flex items-center py-6 justify-end px-6">
               <CardItem
                 translateZ={20}
                 as={Link}
                 href={`portfolio/${album.albumId}`}
-                className="px-4 py-2 text-xs font-normal  hover:underline"
+                className="px-4 py-2 text-xs font-normal  hover:underline transition-all ease-in-out duration-200 hover:text-sky-500"
               >
                 View Album →
               </CardItem>
--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -4,17 +4,29 @@ import dirtucover from "../../public/images/gallery/dirtu/cover.jpg";
 import subarucover from "../../public/images/gallery/subaru/cover.jpg";
 
 const gallery = [
-  { albumId: "brc", title: "Batangas Racing Circuit", cover: brccover },
-  { albumId: "cis", title: "Clark International Speedway", cover: ciscover },
+  {
+    albumId: "brc",
+    title: "Batangas Racing Circuit",
+    cover: brccover,
+    blurDataURL: brccover.blurDataURL,
+  },
+  {
+    albumId: "cis",
+    title: "Clark International Speedway",
+    cover: ciscover,
+    blurDataURL: ciscover.blurDataURL,
+  },
   {
     albumId: "dirtuclinic3",
     title: "Dirt University Rally Clinic 3",
     cover: dirtucover,
+    blurDataURL: dirtucover.blurDataURL,
   },
   {
     albumId: "subarutrackdayexperience",
     title: "Subaru Track Day Experience",
     cover: subarucover,
+    blurDataURL: subarucover.blurDataURL,
   },
 ];
 
